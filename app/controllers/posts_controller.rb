e93f3414c1b07ef4a32b58a8be5f6fd6e13456dc
Rodrigo Gomes <rgomes.rodrigo@gmail.com>
2013-01-14 11:10:38 -0200

    Filter posts by date

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,9 @@ class PostsController < ApplicationController
   # GET /posts
   # GET /posts.json
   def index
-    @posts = Post.all
+    # @posts = Post.all
+
+    @posts = Post.where("time > ?", Date.yesterday)
 
     respond_to do |format|
       format.html # index.html.erb

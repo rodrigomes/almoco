810f28cae2317b448fc2b475e3250ae8a8409d12
Rodrigo Gomes <rgomes.rodrigo@gmail.com>
2013-01-14 10:54:44 -0200

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Almoco::Application.config.secret_token = '1805a2596be6ab60348961b09fe230002b86c6cb86becf6f48a49cf0759483a6626626a151d8e83fa25f93cbfbdd89a56c7fbecdd95e6441498b6ab69037a719'
